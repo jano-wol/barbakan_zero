2af19941626b3bb316e974bac01d833d3616d0f9
Andrzej Polak <jjjkamu@gmail.com>
2023-09-04 19:10:52 +0200

    update python

--- source/scripts/test/quick_train.sh
+++ source/scripts/test/quick_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 source ${PYTHON_FOLDER}/venv/bin/activate
-source ${PYTHON_FOLDER}/selfplay/synchronous_loop_quick.sh np ~/Data/katagomoku_nets/tn3_dev tn3_dev b6c96 1
+source ${PYTHON_FOLDER}/selfplay/synchronous_loop_quick.sh np ~/Data/barbakan_zero/tn3_dev tn3_dev b6c96 1
 
