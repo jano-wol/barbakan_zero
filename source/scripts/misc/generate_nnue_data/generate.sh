1071ad2e8d9f2f615cf730a3cbb8e8bf6c23495b
janowol <janos.wolosz@gmail.com>
2023-10-09 17:58:24 +0200

    First steps to dump input positions

--- source/scripts/misc/generate_nnue_data/generate.sh
+++ source/scripts/misc/generate_nnue_data/generate.sh
@@ -4,11 +4,15 @@ set -ex
 INPUT_SELFPLAY_FOLDER=$1
 OUTPUT_FOLDER=$2
 TARGET_NNUE_DATA_ROWS=$3
+POS_LEN=$4
 
 SCRIPT_FOLDER_GENERATE_NNUE=$(dirname "${0}") 
 WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER_GENERATE_NNUE}/../../../../")
 SOURCE_FOLDER="${WORKSPACE_FOLDER}/source"
 PYTHON_FOLDER="${SOURCE_FOLDER}/python"
 source ${PYTHON_FOLDER}/venv/bin/activate
-python3 ${PYTHON_FOLDER}/generate_nnue_data/shuffle.py ${INPUT_SELFPLAY_FOLDER} -expand-window-per-row 1.0 -taper-window-exponent 1.0 -out-dir ${OUTPUT_FOLDER}/shuffle -out-tmp-dir ${OUTPUT_FOLDER}/shuffle_tmp -num-processes 4 -batch-size 128 -keep-target-rows ${TARGET_NNUE_DATA_ROWS} -min-rows 20000 -output-npz
 
+SHUFFLE_OUT="${OUTPUT_FOLDER}/shuffle"
+DUMP_POSITIONS_OUT="${OUTPUT_FOLDER}/dump_positions_out"
+python3 ${PYTHON_FOLDER}/generate_nnue_data/shuffle.py ${INPUT_SELFPLAY_FOLDER} -expand-window-per-row 1.0 -taper-window-exponent 1.0 -out-dir ${SHUFFLE_OUT} -out-tmp-dir ${OUTPUT_FOLDER}/shuffle_tmp -num-processes 4 -batch-size 128 -keep-target-rows ${TARGET_NNUE_DATA_ROWS} -min-rows 20000 -output-npz
+python3 ${PYTHON_FOLDER}/generate_nnue_data/dump_positions.py -shuffle-dir ${SHUFFLE_OUT} -dump-dir ${DUMP_POSITIONS_OUT} -batch-size 128 -pos-len ${POS_LEN}
--- source/scripts/test/quick_generate_nnue_data.sh
+++ source/scripts/test/quick_generate_nnue_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-${SOURCE_FOLDER}/scripts/misc/generate_nnue_data/generate.sh ${BUILD_TEST_DATA_FOLDER}/generate_nnue_data ${BUILD_TEST_DATA_FOLDER}/generate_nnue_data_out 300000
+${SOURCE_FOLDER}/scripts/misc/generate_nnue_data/generate.sh ${BUILD_TEST_DATA_FOLDER}/generate_nnue_data ${BUILD_TEST_DATA_FOLDER}/generate_nnue_data_out 300000 20
 
