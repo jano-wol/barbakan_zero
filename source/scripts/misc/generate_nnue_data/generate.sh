c6685765d9326ba0ddb33797ccddb669e9f221a0
janowol <janos.wolosz@gmail.com>
2023-10-14 23:17:35 +0200

    Generate middle scaled data

--- source/scripts/misc/generate_nnue_data/generate.sh
+++ source/scripts/misc/generate_nnue_data/generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# example call
+# ./source/scripts/misc/generate_nnue_data/generate.sh ./data/train/selfplay/ ./data/train/out/ 10000000 20 ./data/bin/20
 set -ex
 
 INPUT_SELFPLAY_FOLDER=$1
 OUTPUT_FOLDER=$2
-TARGET_NNUE_DATA_ROWS=$3
+TARGET_NNUE_DATA_ROWS=$3 # ~5% of the rows will form the validation set
 POS_LEN=$4
 NN_ENGINE_FOLDER=$5
 
@@ -12,9 +14,10 @@ WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER_GENERATE_NNUE}/../../../../")
 SOURCE_FOLDER="${WORKSPACE_FOLDER}/source"
 PYTHON_FOLDER="${SOURCE_FOLDER}/python"
 source ${PYTHON_FOLDER}/venv/bin/activate
+TARGET_SHUFFLE=$(( TARGET_NNUE_DATA_ROWS + 1000 ))
 
 SHUFFLE_OUT="${OUTPUT_FOLDER}/shuffle"
-DUMP_POSITIONS_OUT="${OUTPUT_FOLDER}/dump_positions_out"
-python3 ${PYTHON_FOLDER}/generate_nnue_data/shuffle.py ${INPUT_SELFPLAY_FOLDER} -expand-window-per-row 1.0 -taper-window-exponent 1.0 -out-dir ${SHUFFLE_OUT} -out-tmp-dir ${OUTPUT_FOLDER}/shuffle_tmp -num-processes 4 -batch-size 128 -keep-target-rows ${TARGET_NNUE_DATA_ROWS} -min-rows 20000 -output-npz
-python3 ${PYTHON_FOLDER}/generate_nnue_data/dump_positions.py -shuffle-dir ${SHUFFLE_OUT} -dump-dir ${DUMP_POSITIONS_OUT} -batch-size 128 -pos-len ${POS_LEN}
+DUMP_FILE="${OUTPUT_FOLDER}/dump_positions_out"
+python3 ${PYTHON_FOLDER}/generate_nnue_data/shuffle.py ${INPUT_SELFPLAY_FOLDER} -expand-window-per-row 1.0 -taper-window-exponent 1.0 -out-dir ${SHUFFLE_OUT} -out-tmp-dir ${OUTPUT_FOLDER}/shuffle_tmp -num-processes 4 -batch-size 128 -keep-target-rows ${TARGET_SHUFFLE} -min-rows 20000 -output-npz
+python3 ${PYTHON_FOLDER}/generate_nnue_data/dump_positions.py -shuffle-dir ${SHUFFLE_OUT} -dump-file ${DUMP_FILE} -batch-size 128 -pos-len ${POS_LEN} -target ${TARGET_NNUE_DATA_ROWS}
 ${NN_ENGINE_FOLDER}/barbakan_zero generatennuedata ${OUTPUT_FOLDER} ${POS_LEN}
