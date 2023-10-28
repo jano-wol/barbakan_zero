a2e4663fb893edcca69beb5cfd529ab7ed0010a7
janowol <janos.wolosz@gmail.com>
2023-10-28 13:30:50 +0200

    Simplify default net

--- source/scripts/train/loop_call.sh
+++ source/scripts/train/loop_call.sh
@@ -5,6 +5,6 @@ SELFPLAY_FOLDER="${TRAIN_FOLDER}/${2}"
 if [ ! -d "$SELFPLAY_FOLDER" ]; then
   mkdir -p "${SELFPLAY_FOLDER}"
 fi
-NN_ID=b6c96
+NN_ID=b2c16
 source $(dirname "${0}")/train/synchronous_loop.sh np "${SELFPLAY_FOLDER}" ${2} ${NN_ID} 1
 
